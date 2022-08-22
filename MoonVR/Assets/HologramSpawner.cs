c8838ef967756c24b0c1373630043fc9558c1918
Henrynaut <neilmchenry@gmail.com>
2022-08-21 23:17:54 -0500

    WIP hologram spawning over network changes

--- MoonVR/Assets/HologramSpawner.cs
+++ MoonVR/Assets/HologramSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,24 +8,49 @@ public class HologramSpawner : MonoBehaviour
 
     public GameObject hologram;
     public GameObject panel;
+    private PhotonView PV;
+
     int count;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = GetComponent<PhotonView>();
     }
 
 
     public void MakeCopy()
     {
+        if( PhotonNetwork.OfflineMode == true)
+        {
+            MakeCopyHologram();
+        }
+        else
+        {
+            //SplineDraw() RPC Online Mode
+            PV.RPC( "MakeCopyHologram"
+                          , RpcTarget.All
+                          , new object[] {}
+                          );
+        }
+
+    }
+
+    //RPC call to make a hologram copy
+    [PunRPC]
+    public void MakeCopyHologram()
+    {
         if (count == 0)
         {
             GameObject hologramCopy = Instantiate(hologram, panel.transform);
+            //Increment photon view ID by 20
+            int currentID = PV.ViewID;
+            currentID = currentID + 20;
+            PV.ViewID = currentID;
             hologram.transform.parent = null;
+            print("Spawning CHILL ICE Hologram");
             count++;
         }
-
     }
 
     // Update is called once per frame
